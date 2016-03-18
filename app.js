06999883d7dc5e7d2a8a09a4b04eabb92c73874a
Christopher Bisignani <bigsley@gmail.com>
2016-03-17 23:08:06 -0700

    fix some stuff

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ app.get('/', function (req, res) {
 
 app.use(express.static('public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
